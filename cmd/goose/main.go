aad42ef6e0308b87a47455a2a5ce8e31684397ab
Otto Millrath <ottomillrath@gmail.com>
2021-02-01 17:23:08 -0300

    update usageCommands

--- cmd/goose/main.go
+++ cmd/goose/main.go
@@ -161,7 +161,7 @@ Commands:
     reset                Roll back all migrations
     status               Dump the migration status for the current DB
     version              Print the current version of the database
-    create NAME [sql|go] Creates new migration file with the current timestamp
+    create SERVICE NAME [sql|go] Creates new migration file with the current timestamp
     fix                  Apply sequential ordering to migrations
 `
 )
